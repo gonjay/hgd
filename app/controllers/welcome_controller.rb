c444114d7afbabc3b372c3c88e9c40cf1aff8a93
gonjay <assyer@gmail.com>
2013-10-18 09:10:40 +0800

    add .order(created_at DESC) to controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @nav = 1
-    @officialnews = Passage.where(psgType: 1).limit(5)
-    @schoolbully  = Passage.where(psgType: 2).limit(5)
-    @growthbook   = Passage.where(psgType: 3).limit(5)
-    @hearsay      = Passage.where(psgType: 4).limit(5)
+    @officialnews = Passage.where(psgType: 1).order("created_at DESC").limit(5)
+    @schoolbully  = Passage.where(psgType: 2).order("created_at DESC").limit(5)
+    @growthbook   = Passage.where(psgType: 3).order("created_at DESC").limit(5)
+    @hearsay      = Passage.where(psgType: 4).order("created_at DESC").limit(5)
   end
 
   def officialnews
