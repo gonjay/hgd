248f5341b0c9fa6ecf426eb722d813c47b9c7d09
gonjay <assyer@gmail.com>
2013-10-18 19:26:54 +0800

    汉化

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ module Hgd
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = "zh-CN"
-    # I18n.locale = 'zh-CN'
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = "zh-CN"
+    I18n.locale = 'zh-CN'
   end
 end
